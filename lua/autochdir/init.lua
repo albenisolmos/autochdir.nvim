ee4811a17ec45f12fe06dd24325904787d7094b2
albenisolmos <albenisolmos88@gmail.com>
2025-02-26 18:19:15 -0400

    fix: avoid freeze oil.nvim buffer

--- lua/autochdir/init.lua
+++ lua/autochdir/init.lua
@@ -82,7 +82,10 @@ function M.setup(_settings)
 	api.nvim_create_autocmd({ "BufEnter" }, {
 		group = augroup_autochdir,
 		callback = function()
-			if api.nvim_win_get_config(0).relative ~= "" then
+			-- avoid freeze oil.nvim
+			if api.bo.filetype == "oil" then
+				return
+			elseif api.nvim_win_get_config(0).relative ~= "" then
 				block_keep_dir = true
 				return
 			end
